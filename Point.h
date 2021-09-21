0cb214523fbb218ab9a274d469c1fc2f09b2af0b
XingZhiang <xingzhiang2002@126.com>
2021-09-21 19:58:35 +0800

    add class Point

---
+++ Point.h
@@ -0,0 +1,30 @@
+/*
+ * 点类
+ */
+#pragma once
+#ifndef GRAPH_LAB_POINT_H
+#define GRAPH_LAB_POINT_H
+#include <GL/glut.h>
+
+class Point{
+public:
+    int x;
+    int y;
+public:
+    Point(int x_value, int y_value):x(x_value),y(y_value){}
+    Point(Point &p):x(p.x),y(p.y){}
+};
+
+/*
+ **********************************
+ * 点亮像素点函数
+ **********************************
+ */
+void setPixel(int x, int y) {
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+    glFlush();
+}
+#endif //GRAPH_LAB_POINT_H
