4be074a853af4b3d28262df5989abcc8175565f9
cocochick <jiyishen217@gmail.com>
2021-09-21 22:44:38 +0800

    add MidPoint

--- Line.h
+++ Line.h
@@ -5,7 +5,8 @@
 #include <cmath>
 #include "Point.h"
 
-class Line{
+class Line
+{
 private:
     Point start;
     Point end;
@@ -13,73 +14,144 @@ private:
 public:
     Line() = delete;
     Line(int xS, int yS, int xE, int yE);
+
 private:
     static void DDA(int xS, int yS, int xE, int yE);
+    static void MidPoint(int xS, int yS, int xE, int yE);
     static void Bresenham(int xS, int yS, int xE, int yE);
 
 public:
     void drawByDDA() const;
+    void drawByMidPoint() const;
     void drawByBresenham() const;
 };
 
-Line::Line(int xS, int yS, int xE, int yE):
-    start(xS,yS),end(xE,yE){}
+Line::Line(int xS, int yS, int xE, int yE) : start(xS, yS), end(xE, yE) {}
 
-
-void Line::DDA(int xS, int yS, int xE, int yE) {
+void Line::DDA(int xS, int yS, int xE, int yE)
+{
     int dx = xE - xS, dy = yE - yS, steps = 0;
-    float xIncrement, yIncrement, x = static_cast<float >(xS), y = static_cast<float>(yS);
+    float xIncrement, yIncrement, x = static_cast<float>(xS), y = static_cast<float>(yS);
     steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
     xIncrement = float(dx) / float(steps);
     yIncrement = float(dy) / float(steps);
 
-    setPixel(static_cast<int>(x + 0.5), static_cast<int>(x + 0.5));
-    for (int i = 0; i < steps; ++i) {
+    setPixel(static_cast<int>(x + 0.5), static_cast<int>(y + 0.5));
+    for (int i = 0; i < steps; ++i)
+    {
         x += xIncrement;
         y += yIncrement;
         setPixel(static_cast<int>(x + 0.5), static_cast<int>(y + 0.5));
     }
 }
 
-void Line::drawByDDA() const {
+void Line::drawByDDA() const
+{
     DDA(start.x, start.y, end.x, end.y);
 }
 
-void Line::Bresenham(int xS, int yS, int xE, int yE) {
-    int dx = xE - xS, dy = yE - yS;
-    int x = xS, y = yS;
-    setPixel(xS,yS);
-    int ux = dx > 0?1:-1;
-    int uy = dy > 0?1 : -1;
-    dx = abs(dx); dy= abs(dy);
-    if( dx > dy){
-        int p = 2*dy - dx;
-        for(int i = 0; i< abs(dx); ++i){
-            if(p < 0) {
-                x+=ux;
-                p = p + 2*dy;
-            }else{
-                x+=ux; y+=uy;
-                p = p + 2*dy - 2*dx;
+void Line::MidPoint(int xS, int yS, int xE, int yE)
+{                                                                                                                                  //0 50 50 0
+    int a = yS - yE, b = xE - xS, c = xS * yE - xE * yS;                                                                           //直线方程 50 50 -250
+    int u = a * b > 0 ? 0 : 1;                                                                                                     //斜率大于0时u=1，反之=0
+    int delta0 = u == 1 ? a + a + b : a + a - b, delta1 = u == 1 ? a + a : a + a - b - b, delta2 = u == 1 ? a + a + b + b : a + a; //变化量 50 0 100
+    int x = xS, y = yS;                                                                                                            //当前点坐标
+
+    setPixel(x, y);
+    if (abs(a) <= abs(b))
+    { //|k|<=1
+        while (x < xE)
+        {
+            if (delta0 < 0)
+            {
+                x++, y += u;
+                delta0 += delta2;
             }
-            setPixel(x,y);
+            else
+            {
+                x++, y += u - 1;
+                delta0 += delta1;
+            }
+            setPixel(x, y);
         }
-    }else{
-        int p = 2 * dx - dy;
-        for(int i = 0; i< abs(dy); ++i){
-            if(p < 0) {
-                y+=uy;
-                p = p + 2*dx;
-            }else{
-                x+=ux; y+=uy;
-                p = p + 2*dx - 2*dy;
+    }
+    else
+    {
+        delta0 = u == 1 ? b + b + a : -b - b + a, delta1 = u == 1 ? b + b + a + a : -b - b, delta2 = u == 1 ? b + b : -b - b + a + a;
+        int d = u == 1 ? 1 : -1;
+        while (abs(y - yE) > 0)
+        {
+            if (delta0 < 0)
+            { //k>0时x不动，k<0时x+1
+                y += d, x += 1 - u;
+                delta0 += delta2;
             }
-            setPixel(x,y);
+            else
+            {
+                y += d, x += u;
+                delta0 += delta1;
+            }
+            setPixel(x, y);
         }
     }
 }
 
-void Line::drawByBresenham() const {
+void Line::drawByMidPoint() const
+{
+    MidPoint(start.x, start.y, end.x, end.y);
+}
+
+void Line::Bresenham(int xS, int yS, int xE, int yE)
+{
+    int dx = xE - xS, dy = yE - yS;
+    int x = xS, y = yS;
+    setPixel(xS, yS);
+    int ux = dx > 0 ? 1 : -1;
+    int uy = dy > 0 ? 1 : -1;
+    dx = abs(dx);
+    dy = abs(dy);
+    if (dx > dy)
+    {
+        int p = 2 * dy - dx;
+        for (int i = 0; i < abs(dx); ++i)
+        {
+            if (p < 0)
+            {
+                x += ux;
+                p = p + 2 * dy;
+            }
+            else
+            {
+                x += ux;
+                y += uy;
+                p = p + 2 * dy - 2 * dx;
+            }
+            setPixel(x, y);
+        }
+    }
+    else
+    {
+        int p = 2 * dx - dy;
+        for (int i = 0; i < abs(dy); ++i)
+        {
+            if (p < 0)
+            {
+                y += uy;
+                p = p + 2 * dx;
+            }
+            else
+            {
+                x += ux;
+                y += uy;
+                p = p + 2 * dx - 2 * dy;
+            }
+            setPixel(x, y);
+        }
+    }
+}
+
+void Line::drawByBresenham() const
+{
     Bresenham(start.x, start.y, end.x, end.y);
 }
 
